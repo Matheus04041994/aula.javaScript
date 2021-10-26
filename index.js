2d9624ecddee913910f01f653ba9f8eae0489f25
Matheus04041994 <mh.pt@hotmail.com>
2021-10-26 01:32:05 +0000

    save

---
+++ index.js
@@ -0,0 +1,11 @@
+// comentário de uma linha
+/*comentário
+com mais de umalinha*/
+//variáveis 
+var num = 10;
+let num2 = 20;
+const num3 = 30;
+
+
+var op = num > num2;
+console.log("Olá Mundo!!!")
